2f5a168644bef16b6fcebea221aaf0edfaa1e1d0
wuhu <wuhu0723@126.com>
2023-07-20 14:36:32 +0800

    02-插值表达式的使用

--- src/components/day1/02-插值表达式.vue
+++ src/components/day1/02-插值表达式.vue
@@ -1,8 +1,15 @@
 <template>
   <div class='mydiv'>
     <!-- 只有一个根元素 -->
-    <h1>标题</h1>
-    <h3>副标题</h3>
+    <!-- 在标签之间插入一个值--类似于设置标签之间的内容--插值表达式 -->
+    <h1>{{'我的姓名是：'+obj.name.toUpperCase()}}</h1>
+    <h1>{{obj.age}}</h1>
+    <h1>{{obj.gender==1?'男':'女'}}</h1>
+
+    <!-- <h2>{{if(obj.gender==1){'男'}}}</h2> -->
+    <!-- <h2>{{obj.age++}}</h2> -->
+
+    <h2 v-text=''>{{obj.age}}</h2>
   </div>
 </template>
 
@@ -17,7 +24,8 @@ export default {
     return {
       obj: {
         name: 'jack',
-        age: 20
+        age: 20,
+        gender: 1
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import Vue from 'vue'
 // import App from './App.vue'
 // 将路径修改为你当前想要渲染的组件的路径
 // @ 代表src
-import App from '@/components/day1/01-单文件组件的体验.vue'
+// import App from '@/components/day1/01-单文件组件的体验.vue'
+import App from '@/components/day1/02-插值表达式.vue'
 
 // 如果这个包整个项目都需要使用，那么就可以在main.js中进行全局引入--只需要引入一次
 import 'normalize.css'
@@ -17,4 +18,4 @@ Vue.config.productionTip = false
 new Vue({
   // 实现组件的渲染
   render: h => h(App)
-}).$mount('#app') // 将渲染的结果挂载到页面中的app标签
+}).$mount('#app') // 将渲染的结果挂载到html页面中的app标签
